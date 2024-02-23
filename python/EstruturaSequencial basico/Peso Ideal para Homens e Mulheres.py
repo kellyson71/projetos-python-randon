fc9c03a06cd8ea69361e92d108ebb4e0f0cc96fe
kellyson <kellyson.medeiros.pdf@gmail.com>
2024-02-23 07:13:23 -0300

    py

---
+++ python/EstruturaSequencial basico/Peso Ideal para Homens e Mulheres.py
@@ -0,0 +1,5 @@
+altura = float(input("Digite sua altura em metros: "))
+peso_homem = (72.7 * altura) - 58
+peso_mulher = (62.1 * altura) - 44.7
+print(f"Peso ideal para homens: {peso_homem}")
+print(f"Peso ideal para mulheres: {peso_mulher}")
