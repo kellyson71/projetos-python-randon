1f4ee7270bc2755bbd0c02464ab16cd2589dfe9b
kellyson <kellyson.medeiros.pdf@gmail.com>
2024-02-23 07:13:02 -0300

    py

---
+++ python/EstruturaSequencial basico/Média das Notas Bimestrais.py
@@ -0,0 +1,6 @@
+nota1 = float(input("Digite a nota do primeiro bimestre: "))
+nota2 = float(input("Digite a nota do segundo bimestre: "))
+nota3 = float(input("Digite a nota do terceiro bimestre: "))
+nota4 = float(input("Digite a nota do quarto bimestre: "))
+media = (nota1 + nota2 + nota3 + nota4) / 4
+print(f"A média bimestral é: {media}")
