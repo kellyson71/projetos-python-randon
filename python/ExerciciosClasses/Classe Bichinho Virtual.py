15ae07953132d4c8163508e4074dde6c3e10c99c
kellyson <kellyson.medeiros.pdf@gmail.com>
2024-02-23 14:12:37 -0300

    py

---
+++ python/ExerciciosClasses/Classe Bichinho Virtual.py
@@ -0,0 +1,33 @@
+class BichinhoVirtual:
+    def __init__(self, nome, fome, saude, idade):
+        self.nome = nome
+        self.fome = fome
+        self.saude = saude
+        self.idade = idade
+
+    def alterarNome(self, novo_nome):
+        self.nome = novo_nome
+
+    def alterarFome(self, nova_fome):
+        self.fome = nova_fome
+
+    def alterarSaude(self, nova_saude):
+        self.saude = nova_saude
+
+    def alterarIdade(self, nova_idade):
+        self.idade = nova_idade
+
+    def retornarNome(self):
+        return self.nome
+
+    def retornarFome(self):
+        return self.fome
+
+    def retornarSaude(self):
+        return self.saude
+
+    def retornarIdade(self):
+        return self.idade
+
+    def calcularHumor(self):
+        return self.fome + self.saude
