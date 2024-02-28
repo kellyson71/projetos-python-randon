34527293c2873af2fbbf7813eb1897434798d09b
kellyson <kellyson.medeiros.pdf@gmail.com>
2024-02-28 07:27:15 -0300

    py

---
+++ python/ExerciciosClasses/Classe Pessoa.py
@@ -0,0 +1,20 @@
+class Pessoa:
+    def __init__(self, nome, idade, peso, altura):
+        self.nome = nome
+        self.idade = idade
+        self.peso = peso
+        self.altura = altura
+
+    def envelhecer(self):
+        self.idade += 1
+        if self.idade < 21:
+            self.altura += 0.5
+
+    def engordar(self, peso):
+        self.peso += peso
+
+    def emagrecer(self, peso):
+        self.peso -= peso
+
+    def crescer(self, altura):
+        self.altura += altura
